61c36c7ae29b03ea64f945ee857a38cc9a95dded
Noseflute <30875233+Noseflute@users.noreply.github.com>
2017-08-28 08:48:00 -1000

    Object Instructor Name change
    August 28th

---
+++ Objectinstuctor.js
@@ -0,0 +1,10 @@
+var newName = prompt ("What is his new first name?");
+
+var cole = {
+	firstName:"Cole",
+  lastName:"House",
+  age: 15,
+  grade: 10
+}
+window.alert(cole.firstName = newName);
+window.alert(cole.firstName);

7e699b8ff50afa23209e3cadd55186400a26beb6
Noseflute <30875233+Noseflute@users.noreply.github.com>
2017-08-30 08:32:58 -1000

    friends.js (Objects)
    8/30/17

---
+++ friends.js
@@ -0,0 +1,24 @@
+var Kevin ={
+	name: "Kevin",
+  age: 17,
+  grade: 12,
+ 	friends: ["Kevin", "Tyler", "Justin"]
+};
+
+var Tyler ={
+	name: "Tyler",
+  age: 17,
+  grade: 12,
+ 	friends: ["Brandon", "Matthew", "Bryson"]
+};
+
+var Justin ={
+	name: "Justin",
+  age: 16,
+  grade: 12,
+ 	friends: ["Teruya", "Jensen", "Aaron"]
+};
+
+var x =[Kevin, Tyler, Justin];
+
+window.alert(x[1].friends[0]);
